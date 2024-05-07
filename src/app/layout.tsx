8730326a42c200f41280a935ca95526e0821d852
Andrea Pasquetto <73109451+andreapasquetto@users.noreply.github.com>
2024-05-07 20:31:58 +0200

    feat: add query client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import QueryProvider from "@/common/query-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
@@ -17,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
---
+++ src/common/query-provider.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | null = null;
+
+function getQueryClient() {
+  if (typeof window === "undefined") {
+    return makeQueryClient();
+  }
+
+  if (!browserQueryClient) {
+    browserQueryClient = makeQueryClient();
+  }
+
+  return browserQueryClient;
+}
+
+interface QueryProviderProps {
+  children: ReactNode;
+}
+
+export default function QueryProvider({ children }: QueryProviderProps) {
+  const queryClient = getQueryClient();
+
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+}
