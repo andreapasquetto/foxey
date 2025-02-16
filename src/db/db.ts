8b890791e543aa82c8b58e265782102f53aa12e5
Andrea Pasquetto <73109451+andreapasquetto@users.noreply.github.com>
2025-02-16 23:18:12 +0100

    refactor: change drizzle config to enable relational queries

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,3 +1,8 @@
+import * as accountingSchema from "@/db/schema/accounting";
+import * as contactsSchema from "@/db/schema/contacts";
+import * as eventsSchema from "@/db/schema/events";
+import * as mobilitySchema from "@/db/schema/mobility";
+import * as placesSchema from "@/db/schema/places";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
@@ -7,4 +12,12 @@ if (!process.env.POSTGRES_CONNECTION_STRING) {
 
 const connection = postgres(process.env.POSTGRES_CONNECTION_STRING);
 
-export const db = drizzle(connection);
+export const db = drizzle(connection, {
+  schema: {
+    ...accountingSchema,
+    ...contactsSchema,
+    ...eventsSchema,
+    ...mobilitySchema,
+    ...placesSchema,
+  },
+});
