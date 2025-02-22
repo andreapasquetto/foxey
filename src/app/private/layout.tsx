e1c297ff5fe9f58f6c3717ce881c027b56438ed5
Andrea Pasquetto <73109451+andreapasquetto@users.noreply.github.com>
2025-02-23 00:40:33 +0100

    refactor: redesign header and navbar

--- src/app/private/layout.tsx
+++ src/app/private/layout.tsx
@@ -1,10 +1,52 @@
-import { NavbarDesktop, NavbarMobile } from "@/components/navbar";
+import {
+  accountingRoute,
+  contactsRoute,
+  eventsRoute,
+  mobilityRoute,
+  placesRoute,
+  privateRoute,
+} from "@/common/routes";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Skeleton } from "@/components/ui/skeleton";
 import { UserButton } from "@clerk/nextjs";
-import { Menu } from "lucide-react";
+import { Calendar, Car, Contact, Landmark, Map, Menu, PocketKnife, Triangle } from "lucide-react";
+import Link from "next/link";
 import { ReactNode } from "react";
+
+const navigationItems = [
+  {
+    href: accountingRoute,
+    icon: <Landmark className="h-5 w-5" />,
+    title: "Accounting",
+  },
+  {
+    href: mobilityRoute,
+    icon: <Car className="h-5 w-5" />,
+    title: "Mobility",
+  },
+  {
+    href: contactsRoute,
+    icon: <Contact className="h-5 w-5" />,
+    title: "Contacts",
+  },
+  {
+    href: eventsRoute,
+    icon: <Calendar className="h-5 w-5" />,
+    title: "Events",
+  },
+  {
+    href: placesRoute,
+    icon: <Map className="h-5 w-5" />,
+    title: "Places",
+  },
+];
 
 export default function PrivateLayout({
   children,
@@ -12,28 +54,45 @@ export default function PrivateLayout({
   children: ReactNode;
 }>) {
   return (
-    <div className="flex min-h-screen w-full flex-col bg-background">
-      <NavbarDesktop />
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-        <header className="sticky top-0 z-30 flex h-14 items-center justify-between gap-4 border-b px-4 backdrop-blur supports-[backdrop-filter]:bg-background/50 sm:static sm:h-auto sm:border-0 sm:px-6">
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button size="icon" variant="outline" className="sm:hidden">
+    <div className="min-h-screen w-full bg-background sm:space-y-6">
+      <header className="sticky top-0 z-30 flex h-16 items-center justify-between border-b px-4 backdrop-blur supports-[backdrop-filter]:bg-background/50 sm:px-6">
+        <Link href={privateRoute}>
+          <PocketKnife className="h-5 w-5" />
+        </Link>
+        <div className="flex items-center gap-3">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button size="icon" variant="outline">
                 <Menu className="h-5 w-5" />
                 <span className="sr-only">Toggle Menu</span>
               </Button>
-            </SheetTrigger>
-            <SheetContent side="left" className="sm:max-w-xs">
-              <NavbarMobile />
-            </SheetContent>
-          </Sheet>
-          <div className="flex items-center gap-3 sm:hidden">
-            <ThemeToggle />
-            <UserButton />
-          </div>
-        </header>
-        <main className="container p-4 sm:px-6 sm:py-0">{children}</main>
-      </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent side="bottom" className="w-[250px]">
+              {navigationItems.map((item) => (
+                <DropdownMenuItem key={item.href} asChild>
+                  <Link
+                    href={item.href}
+                    className="flex h-12 w-full cursor-pointer items-center justify-between gap-1 sm:h-10"
+                    prefetch
+                  >
+                    {item.title} {item.icon}
+                  </Link>
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <ThemeToggle />
+          <UserButton
+            appearance={{
+              elements: {
+                userButtonTrigger: "h-10 w-10",
+              },
+            }}
+            fallback={<Skeleton className="h-7 w-7 rounded-full" />}
+          />
+        </div>
+      </header>
+      <main className="container p-4 sm:px-6 sm:py-0">{children}</main>
     </div>
   );
 }
--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -1,5 +1,17 @@
 import { Heading1 } from "@/components/typography";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function PrivateHome() {
-  return <Heading1>Hello World</Heading1>;
+export default async function PrivateHome() {
+  const user = await currentUser();
+
+  if (!user) {
+    redirect("/sign-in");
+  }
+
+  return (
+    <div className="space-y-12">
+      <Heading1>Hi, {user.fullName ?? user.username ?? "Mysterious User"}</Heading1>
+    </div>
+  );
 }
