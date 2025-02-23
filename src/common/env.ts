77c900bb241221edd528814083685c25ef0aa5d1
Andrea Pasquetto <73109451+andreapasquetto@users.noreply.github.com>
2025-02-23 01:08:09 +0100

    fix: remove database host ip validation from env schema

--- src/common/env.ts
+++ src/common/env.ts
@@ -4,7 +4,7 @@ const envSchema = z
   .object({
     POSTGRES_USER: z.string().min(1),
     POSTGRES_PASSWORD: z.string().min(1),
-    POSTGRES_HOST: z.string().ip({ version: "v4" }).or(z.literal("localhost")),
+    POSTGRES_HOST: z.string().min(1),
     POSTGRES_PORT: z.union([z.string(), z.number()]).pipe(z.coerce.number().int()),
     POSTGRES_DBNAME: z.string().min(1),
     POSTGRES_CONNECTION_STRING: z.string().min(1),
